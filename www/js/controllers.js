ae79961834b5a641b620813c6a36f13ddb907761
marko fluit <marko.fluit@ing.nl>
2016-02-25 18:17:15 +0100

    Implemented Add to favorites via Dishdetails

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -152,7 +152,7 @@ angular.module('conFusion.controllers', [])
             };
         }])
 
-        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', 'baseURL', function($scope, $stateParams, menuFactory, baseURL) {
+        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicPopover', function($scope, $stateParams, menuFactory, favoriteFactory, baseURL, $ionicPopover) {
             
             $scope.baseURL = baseURL;            
             $scope.dish = {};
@@ -169,6 +169,37 @@ angular.module('conFusion.controllers', [])
                                 $scope.message = "Error: "+response.status + " " + response.statusText;
                             }
             );
+            
+            // .fromTemplateUrl() method
+            $ionicPopover.fromTemplateUrl('templates/dish-detail-popover.html', {
+                scope: $scope
+            }).then(function(popover) {
+                $scope.popover = popover;
+            });
+            
+            $scope.openPopover = function($event) {
+                $scope.popover.show($event);
+            };
+            $scope.closePopover = function() {
+                $scope.popover.hide();
+            };
+            //Cleanup the popover when we're done with it!
+            $scope.$on('$destroy', function() {
+                $scope.popover.remove();
+            });
+            // Execute action on hide popover
+            $scope.$on('popover.hidden', function() {
+                // Execute action
+            });
+              // Execute action on remove popover
+            $scope.$on('popover.removed', function() {
+                // Execute action
+            });
+            
+            $scope.addFavorite = function() {
+                favoriteFactory.addToFavorites($scope.dish.id);
+                $scope.closePopover();
+            };
 
             
         }])
